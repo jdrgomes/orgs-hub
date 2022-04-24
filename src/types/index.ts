9f87f323960c6a595ec595d9f9896f34e902432b
Jader <jjadergomes@gmail.com>
2022-04-24 16:28:51 -0300

    chore: create type to org detail list

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,3 +12,35 @@ export type Organization = {
   avatar_url: string
   description: string
 }
+
+export type OrgDetail = {
+  login: string
+  id: number
+  node_id: string
+  url: string
+  repos_url: string
+  events_url: string
+  hooks_url: string
+  issues_url: string
+  members_url: string
+  public_members_url: string
+  avatar_url: string
+  description: string
+  name: string
+  company: null
+  blog: string
+  location: string
+  email: string
+  twitter_username: string
+  is_verified: false
+  has_organization_projects: boolean
+  has_repository_projects: boolean
+  public_repos: number
+  public_gists: number
+  followers: number
+  following: number
+  html_url: string
+  created_at: string
+  updated_at: string
+  type: string
+}
